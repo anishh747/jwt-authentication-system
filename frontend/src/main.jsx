8c4aa7e0d3f1583111ab1d8d3bb54e8f1f7cff11
Anish <anishtimalsina747@gmail.com>
2023-09-07 19:38:52 +0545

    Added Home, Login and Register Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import store from './store.js'
+import { Provider } from 'react-redux'
+import HomeScreen from './screens/HomeScreen.jsx'
+import Login from './components/Login.jsx'
+import Register from './components/Register.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router}/>
+    </React.StrictMode>,
+  </Provider>
 )
